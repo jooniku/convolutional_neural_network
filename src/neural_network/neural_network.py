fc1e8aacb2f01ece4cc440540da256a49f79f051
Joona Nikula <nikulajoona@gmail.com>
2023-09-15 10:22:15 +0300

    initialize project

---
+++ src/neural_network/neural_network.py
@@ -0,0 +1,10 @@
+class NeuralNetwork:
+    """
+    Class for the neural network. The layers are represented as subclasses. 
+    Hyperparameters are class objects.
+    """
+    def __init__(self) -> None:
+        pass
+
+
+
