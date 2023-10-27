262479afa4a126c3a766124ea55d36b90778ecae
jooniku <joona.nikula@helsinki.fi>
2023-10-27 09:35:55 +0300

    .

--- src/network/non_linearity.py
+++ src/network/non_linearity.py
@@ -13,10 +13,13 @@ class NonLinearity:
         """Leaky rectified linear unit (ReLU) function to increase
         non-linearity. 
         """
+        activation = np.zeros_like(images)
         height, width = images[0].shape
         for filtered_img in range(len(images)):
             for row in range(height):
                 for col in range(width):
+                    value = images[filtered_img][row][col]
                     if images[filtered_img][row][col] < 0:
-                        images[filtered_img][row][col] *= 0.00001
-        return images
+                        value *= 0.00001
+                    activation[filtered_img][row][col] += value
+        return activation
