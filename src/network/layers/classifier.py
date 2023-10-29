fix relu backpropagation bug

src/network/layers/classifier.py
@@ -16,7 +16,7 @@ class Classifier:
 
         exp_scores = np.exp(image)
         probabilities = (
-            exp_scores / np.sum(exp_scores, axis=0, keepdims=True))
+            exp_scores / np.sum(exp_scores))
 
         return probabilities
 
@@ -31,7 +31,7 @@ class Classifier:
         Returns:
             Loss : Total loss over the complete batch
         """
-        loss = -np.log(probabilities[label])
+        loss = -np.sum(np.log(probabilities[label]))
 
         return loss
 
@@ -41,6 +41,6 @@ class Classifier:
         """
         probabilities[label] -= 1
 
-        gradients = np.array(probabilities)
+        gradients = np.array(probabilities).reshape(1, 10)
 
         return gradients

src/network/layers/convolutional_layer.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.linalg import toeplitz
 
 
 class ConvolutionalLayer:
@@ -8,7 +7,6 @@ class ConvolutionalLayer:
         self.filter_size = filter_size
         self.num_of_filters = num_of_filters
         self.stride_length = stride_length
-
         self._create_filters()
 
     def _create_filters(self):
@@ -22,15 +20,17 @@ class ConvolutionalLayer:
 
         self.bias_vector = [0.01 for i in range(self.num_of_filters)]
 
-    def _add_padding(self, image: np.array):
+    def add_padding(self, image: np.array):
         """Adds zero-padding for the image to make sure the
         operations work. All images are padded to be the
         same shape as the original input.
         """
-        # needed_padding = int(np.ceil(((self.stride_length - 1)*self.conv_in_shape[1]
-        #                          -self.stride_length+self.filter_size)/2))
-
-        needed_padding = (28 - len(image)) // 2
+        return image
+        needed_padding = int(np.ceil(((self.stride_length - 1)*28
+                                  -self.stride_length+self.filter_size)/2))
+        #needed_padding = 0
+        #if image.shape[1] < 32:
+        #    needed_padding = int(np.ceil((32 - len(image)) / 2))
 
         return np.pad(image, pad_width=needed_padding)
 
@@ -40,7 +40,7 @@ class ConvolutionalLayer:
         all filters in the convolutional layer and
         returns a 3D array with each filter activation.
         """
-        images = np.array([self._add_padding(image) for image in images])
+        images = np.array([self.add_padding(image) for image in images])
         self.received_inputs = images
 
         output_dim = int(
@@ -85,15 +85,13 @@ class ConvolutionalLayer:
         """Updates the parameters with stored gradients
         from the backpropagation process.
         """
-        self.gradient_filters /= batch_size
-        self.bias_gradients /= batch_size
 
         for filter_i in range(len(self.filters)):
             # Update moment vectors
             self.filter_mean_grad[filter_i] = beta1*self.filter_mean_grad[filter_i] \
-                + (1-beta1)*self.gradient_filters[filter_i]
+                + (1-beta1)*self.gradient_filters[filter_i]/batch_size
             self.filter_grad_variance[filter_i] = beta2*self.filter_grad_variance[filter_i] \
-                + (1-beta2) * self.gradient_filters[filter_i]**2
+                + (1-beta2) * (self.gradient_filters[filter_i]/batch_size)**2
             # Take the bias-corrected variables
             filter_mhat = self.filter_mean_grad[filter_i] / \
                 (1 - beta1**(iterations+1))
@@ -105,9 +103,9 @@ class ConvolutionalLayer:
 
             # Same for bias
             self.bias_mean_grad[filter_i] = beta1*self.bias_mean_grad[filter_i] \
-                + (1-beta1)*self.bias_gradients[filter_i]
+                + (1-beta1)*self.bias_gradients[filter_i]/batch_size
             self.bias_grad_variance[filter_i] = beta2*self.bias_grad_variance[filter_i] \
-                + (1-beta2) * self.bias_gradients[filter_i]**2
+                + (1-beta2) * (self.bias_gradients[filter_i]/batch_size)**2
             bias_mhat = self.bias_mean_grad[filter_i] / \
                 (1 - beta1**(iterations+1))
             bias_vhat = self.bias_grad_variance[filter_i] / \

src/network/layers/fully_connected_layer.py
@@ -8,7 +8,7 @@ class FullyConnectedLayer:
     the classifier also.
     """
 
-    def __init__(self, num_of_classes, input_image_shape) -> None:
+    def __init__(self, num_of_classes, input_image_shape, non_linearity):
         self.number_of_classes = num_of_classes
         self.weight_matrixes = []
         self.biases = []
@@ -17,6 +17,7 @@ class FullyConnectedLayer:
         self.num_dense_layers = 2
 
         self.classifier_function = Classifier()
+        self.non_linearity = non_linearity
 
         self.initialize_weight_matrix()
 
@@ -30,6 +31,8 @@ class FullyConnectedLayer:
             self.received_inputs.append(flattened_image)
             flattened_image = np.dot(
                 flattened_image, self.weight_matrixes[i]) + self.biases[i]
+            if i < len(self.weight_matrixes)-1:
+                flattened_image = self.non_linearity.forward(flattened_image, "fc_layer")
         return flattened_image
 
     def initialize_weight_matrix(self):
@@ -121,6 +124,10 @@ class FullyConnectedLayer:
 
         gradient_for_next_layer = np.dot(gradient_score,
                                          self.weight_matrixes[-1].T)
+        
+        gradient_for_next_layer = gradient_for_next_layer.reshape(-1, 1)
+        gradient_for_next_layer = self.non_linearity.backpropagation(gradient_for_next_layer, 
+                                                                       "fc_layer", 0)
 
         self.received_inputs[-2] = np.array(
             self.received_inputs[-2]).reshape(1, -1)

src/network/neural_network.py
@@ -26,12 +26,11 @@ class NeuralNetwork:
                  learning_rate=0.001,
                  epochs=1,
                  reg_strength=0,
-                 batch_size=50,
+                 batch_size=32,
                  num_of_classes=10,
-                 beta1=0.95,
+                 beta1=0.9,
                  beta2=0.999):
 
-        # hyperparameter initialization here
         self.filter_size = filter_size
         self.stride_length = stride_length
         self.num_of_convolution_layers = num_of_convolutional_layers
@@ -43,7 +42,7 @@ class NeuralNetwork:
         self.num_of_classes = num_of_classes
         self.beta1 = beta1
         self.beta2 = beta2
-        self.learning_rate_schedule = [70]
+        self.learning_rate_schedule = [50, 600]
 
         self._initialize_custom_functions()
 
@@ -66,8 +65,8 @@ class NeuralNetwork:
         distribution of the classes.
         """
         for conv_layer in self.convolutional_layers:
-            images = self.non_linearity_function(
-                conv_layer.add_2d_convolution(images))
+            images = conv_layer.add_2d_convolution(images)
+            images = self.non_linear.forward(images, "conv_layer")
 
         images = self.pooling_layer.average_pooling(images)
 
@@ -113,8 +112,7 @@ class NeuralNetwork:
         """
         self._initialize_plots()
         self.iterations = 1
-        batch_iterations = 1
-        prev_validation_accuracy = 0
+        val_accuracy = 0
 
         for epoch in range(1, self.epochs+1):
             np.random.shuffle(self.training_data)
@@ -135,10 +133,8 @@ class NeuralNetwork:
 
                 self._update_network_parameters()
 
-                # Every 2nd iteration test validation accuracy
-                if batch_iterations % 3 == 0:
+                if self.iterations % 10 == 0:
                     val_accuracy = self._test_validation_accuracy()
-                    prev_validation_accuracy = val_accuracy
 
                     # save network incase overfitting
                     if val_accuracy > 98:
@@ -146,17 +142,16 @@ class NeuralNetwork:
                         self._save_plots()
 
                 self.loss_values.append(average_loss)
-                self.batch_values.append(batch_iterations)
-                self.validation_accuracy.append(prev_validation_accuracy)
+                self.batch_values.append(self.iterations)
+                self.validation_accuracy.append(val_accuracy)
 
                 progress.set_description("Loss: %.2f" % (self.loss_values[-1]))
                 self._plot_data()
 
                 self.iterations += 1
-                batch_iterations += 1
-                
-                if self.iterations in self.learning_rate_schedule:
-                    self.learning_rate *= 0.1
+                if self.iterations >= 400:
+                    if self.iterations % 100 == 0:
+                        self.learning_rate *= 0.1
 
             print("epoch:", epoch)
         self._stop_training(save_network)
@@ -188,13 +183,15 @@ class NeuralNetwork:
         gradient_input = self.fully_connected_layer.backpropagation(
             gradient_score=gradients, reg_strength=self.regularization_strength)
 
-        output_shape = 13
+        output_shape = 6
 
         gradient_input = self.pooling_layer.backpropagation_average_pooling(
             gradient_input, output_shape)
 
         for conv_layer in range(len(self.convolutional_layers)-1, -1, -1):
-            gradient_input = self.non_linearity_function(gradient_input)
+            gradient_input = self.non_linear.backpropagation(gradient_input,
+                                                                  "conv_layer",
+                                                                    conv_layer)
             gradient_input = self.convolutional_layers[conv_layer].backpropagation(
                 gradient_input, self.regularization_strength)
 
@@ -220,10 +217,11 @@ class NeuralNetwork:
         self._get_training_data()
         self._get_validation_data()
 
-        self.non_linearity_function = NonLinearity()._relu
+        self.non_linear = NonLinearity()
         self.pooling_layer = PoolingLayer(kernel_size=2, stride=1)
         self.fully_connected_layer = FullyConnectedLayer(
-            self.num_of_classes, (self.num_of_filters_in_conv_layer, 12, 12))  # if pooling stride 1
+            self.num_of_classes, (self.num_of_filters_in_conv_layer, 5, 5),
+            self.non_linear)  # if pooling stride 1
         self.classifier = Classifier()
 
         self._create_convolutional_layers()
@@ -361,7 +359,7 @@ class NeuralNetwork:
         for conv_layer in self.convolutional_layers:
             input_image = conv_layer.add_2d_convolution(input_image)
             activations.append(input_image)
-            input_image = self.non_linearity_function(input_image)
+            input_image = self.non_linear.forward(input_image, "conv_layer")
             activations.append(input_image)
 
         input_image = self.pooling_layer.average_pooling(input_image)

src/network/non_linearity.py
@@ -7,19 +7,23 @@ class NonLinearity:
     """
 
     def __init__(self):
-        pass
+        self.received_inputs = {"conv_layer": [],
+                                "fc_layer": []}
 
-    def _relu(self, images):
+    def forward(self, image, layer_name):
         """Leaky rectified linear unit (ReLU) function to increase
         non-linearity. 
         """
-        activation = np.zeros_like(images)
-        height, width = images[0].shape
-        for filtered_img in range(len(images)):
-            for row in range(height):
-                for col in range(width):
-                    value = images[filtered_img][row][col]
-                    if images[filtered_img][row][col] < 0:
-                        value *= 0.00001
-                    activation[filtered_img][row][col] += value
-        return activation
+        self.received_inputs[layer_name].append(image)
+        image[image <= 0] = 0.001
+        return image
+    
+    def backpropagation(self, gradient, layer_name, layer_pos):
+        """Backpropagation through ReLU function. If the 
+        image during forward pass was less than 0, the
+        gradient becomes a small constant. Layer position
+        is referring to the layer after which the ReLU is done.
+        """
+        gradient[self.received_inputs[layer_name][layer_pos] <= 0] = 0.001
+
+        return gradient